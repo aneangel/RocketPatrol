fd23aad62ff6f865a948e63eaf4e86fee712a798
An-geles <66815800+An-geles@users.noreply.github.com>
2023-10-11 13:35:02 -0700

    update: went through more of the tutorial

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -3,14 +3,44 @@ class Play extends Phaser.Scene {
         super("playScene");
     }
 
+    preload() {
+        //load images
+        this.load.image('rocket', './assets/rocket.png');
+        this.load.image('spaceship', './assets/spaceship.png');
+        this.load.image('starfield', './assets/starfield.png');
+
+        //load sprite
+        this.load.spritesheet('explosion', './assets/explosion.png',
+            {
+                frameHeight: 32, startFrame: 0, frameWidth: 64, endFrame: 9
+            });
+    }
     create() {
+        // title sprite
+        this.starfield = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0, 0)
         // green UI background
-        this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0, 0);
+        this.add.rectangle(0, borderUISize + borderPadding, game.config.width,
+            borderUISize * 2, 0x00FF00).setOrigin(0, 0);
 
         // white borders
         this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
-        this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
+        this.add.rectangle(0, game.config.height - borderUISize, game.config.width,
+            borderUISize, 0xFFFFFF).setOrigin(0, 0);
         this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
-        this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
+        this.add.rectangle(game.config.width - borderUISize, 0,
+            borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
+
+        this.p1Rocket = new Rocket(this. game.config.width/2,
+            game.config.height - borderUISize -borderPadding, 'rocket'.setOrigin(0.5, 0));
+
+        //define keys
+        keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
+        keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
+        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+    }
+
+    update() {
+        this.starfield.titlePositionX -= 4;
     }
 }
